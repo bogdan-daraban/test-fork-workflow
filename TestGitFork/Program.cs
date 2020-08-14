1be845d2a9453112cd9c2d75b19ed364224faf80
test-github132 <testgithub132@gmail.com>
2020-08-14 16:17:47 +0300

    Added a brand new line

--- TestGitFork/Program.cs
+++ TestGitFork/Program.cs
@@ -7,6 +7,7 @@ namespace TestGitFork
         static void Main(string[] args)
         {
             Console.WriteLine("We are now on the feature branch with a brand new message");
+            Console.WriteLine("We are now on the feature branch with a brand new message");
         }
     }
 }
